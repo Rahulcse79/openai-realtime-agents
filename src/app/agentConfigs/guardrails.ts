757c1f760434cd21b4f9c2d5120b16103689c388
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2026-01-01 18:08:52 +0530

    fix responce issue

--- src/app/agentConfigs/guardrails.ts
+++ src/app/agentConfigs/guardrails.ts
@@ -29,7 +29,7 @@ export async function runGuardrailClassifier(
     },
   ];
 
-  const response = await fetch("http://localhost:3000/api/responses", {
+    const response = await fetch("/api/responses", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
